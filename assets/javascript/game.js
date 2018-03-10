ca6cfa7b17dd212726305d5fbe2163a44c1cf704
Cecilia Clohisy <ceciliaclohisy@Cecilias-MacBook-Pro.local>
2018-03-10 12:25:36 -0600

    psuedocode done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,2 +1,55 @@
 
+//establish variables
+    //compScore
+    //userScore
+    //win
+    //loss
+    //result
+    //set id for each button?, buttons given class of crystal...
+        //display initial values of..
+            //compScore
+            //userScore
+            //win
+            //loss
+
+
+//establish possible computer scores vaules 19-120
+    //mathfloor(mathrandom()) * 102 )+19
+    //display inital computer score 
+
+// establish random crystal vaules 1-12 
+        //math floor(mathrandom() 12 ) +1
+        //assign different random value to each crystal (4 diff vaules for four crystals)
+        
+
+//create function to reset game - will run after user win or loss
+    //generate new random computer score 
+         //display new computer score 
+    //set user score to 0 
+        //display new user score 
+    // generate new random vaules for each crystal
+
+//create function to run on user click of crystals userScore    
+        //IF user score < computer score 
+            // when user clicks crystal
+                //add value of crystal clicked to previous userScore
+                //display new value of userScore in #userScore
+
+        //ELSE IF user score == computer score 
+            // increase win +1
+                //display new vaule of win in #win 
+            //display you won result message in #result
+            //run reset function
+
+        //ELSE 
+            // increase loss +1
+                //display new value of loss in #loss
+            //display you lost result message in #result
+            //run rest function
+
+
+
+
+
+
 
